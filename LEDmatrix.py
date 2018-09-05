c63b465913a4171ea9f3dfbd5f33aae16887d6d0
eykei <kuo.austin@yahoo.com>
2018-09-04 19:50:29 -0700

    8th commit

--- LEDmatrix.py
+++ LEDmatrix.py
@@ -19,17 +19,24 @@ class App(SampleBase):
         self.canvas = self.matrix.CreateFrameCanvas()
         while True:
             self.initializeDataPoints()
+            self.canvas.Clear()
+            self.draw()
+            self.matrix.SwapOnVSync(self.canvas)
+            time.sleep(3)
             while True:
                 self.canvas.Clear()
-                for dp in self.dpList:
-                    self.draw(dp)
-                self.canvas = self.matrix.SwapOnVSync(self.canvas)
-                self.bubbleSortStep()
+                self.draw()
+                self.matrix.SwapOnVSync(self.canvas)
                 if self.isSorted():
+                    time.sleep(5)
                     break
-                time.sleep(0.5)
+                else:
+                    #self.bubbleSortStep()
+                    self.selectionSortStep()
+                time.sleep(0.1)
+                
 
-    def draw(self, datapoint):
+    def draw(self):
         for dp in self.dpList:
             for y in range(dp.value):
                 self.canvas.SetPixel(dp.position, 62-y, 200,200,200)
@@ -57,6 +64,14 @@ class App(SampleBase):
             n+=1
             if self.dpList[i].value > self.dpList[i+1].value:
                 self.dpList[i].value, self.dpList[i+1].value = self.dpList[i+1].value, self.dpList[i].value
+                
+    def selectionSortStep(self):
+        min_index = 0
+        for i in range(MATRIXLENGTH):
+            min = self.dpList[min_index].value
+            if self.dpList[i].value < min:
+                self.dpList[i].value, self.dpList[min_index].value = self.dpList[min_index].value, self.dpList[i].value
+                min_index += 1
 
 if __name__ == "__main__":
     app = App()
