add44d276c1f1abc89ac95dd480d0e672ab04ebe
eykei <statuskuo2590@yahoo.com>
2018-11-15 21:13:01 -0800

    debug

--- visualSort.py
+++ visualSort.py
@@ -84,7 +84,7 @@ class App(SampleBase):
             for j in range(i+1, len(self.dpList)-1):
                 if self.dpList[min_index].value > self.dpList[j].value:
                     min_index = j
-            self.dpList[j].value, self.dpList[min_index].value = self.dpList[min_index].value, self.dpList[j].value
+            self.dpList[i].value, self.dpList[min_index].value = self.dpList[min_index].value, self.dpList[i].value
 
         self.draw(GREEN)
         time.sleep(5)
